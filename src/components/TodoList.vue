b91da69f70a838958ba8e6bdf65ab9642ed846de
ziinup <wlsgmlxo@gmail.com>
2022-05-03 18:06:21 +0900

    constructmodifying

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,66 @@
 <template>
-    <div>List</div>
+    <ul>
+        <li v-for="(todoItem, index) in todoItems" :key="todoItem" class="shadow">
+            <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+            {{todoItem}}
+            <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                <i class="fa fa-trash-o" aria-hidden="true"></i>
+            </span>
+        </li>
+    </ul>
 </template>
 
 <script>
 export default ({
-    name:'TodoInput',
-
+    name:'TodoList',
+    data(){
+        return {
+            todoItems: []
+        }
+    },
+    created(){
+        if(localStorage.length>0){
+            for(var i=0;localStorage.length>i;i++){
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    },
+    methods:{
+        removeTodo(todoItem, index){
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index,1);
+        }
+    }
 })
 </script>
 
-<style scoped>
+<style>
+ul {
+    list-style-type:none;
+    padding-left:0px;
+    margin-top:0;
+    text-align:left;
+}
 
+li {
+    display:flex;
+    min-height:50px;
+    height:50px;
+    line-height:50px;
+    margin:0.5rem 0;
+    padding:0 0.9rem;
+    background:white;
+    border-radius:5px;
+}
+
+.checkBtn {
+    line-height:45px;
+    color:#62acde;
+    margin-right:5px;
+}
+
+.remobeBtn {
+    margin-left:auto;
+    color:#de4343;
+}
 </style>
